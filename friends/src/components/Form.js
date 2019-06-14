884641dd358cec1ee2512d50965f10b7bca0f00c
Braden Mason <bradenemason@gmail.com>
2019-06-14 18:15:52 -0500

    moved form and friendslist to seperate components, implemented route

--- friends/src/components/Form.js
+++ friends/src/components/Form.js
@@ -11,21 +11,21 @@ function Form(props) {
           placeholder="name"
           name="name"
           onChange={props.handleChange}
-          value={props.state.name}
+          value={props.name}
         />
         <input
           type="number"
           placeholder="age"
           name="age"
           onChange={props.handleChange}
-          value={props.state.age}
+          value={props.age}
         />
         <input
           type="text"
           placeholder="email"
           name="email"
           onChange={props.handleChange}
-          value={props.state.email}
+          value={props.email}
         />
         <button type="submit">Add</button>
       </form>
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,79 +1,21 @@
 import React from "react";
-import axios from "axios";
 
-class FriendsList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      friends: [],
-      name: "",
-      age: "",
-      email: ""
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get("http://localhost:5000/friends")
-      .then(response => this.setState({ friends: response.data }))
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  handleChange = e => {
-    e.persist();
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(this.state);
-
-    const newFriend = {
-      name: this.state.name,
-      age: this.state.age,
-      email: this.state.email
-    };
-
-    axios.post("http://localhost:5000/friends", newFriend).then(response => {
-      this.setState({ friends: response.data });
-      console.log(response.data);
-    });
-    this.setState({
-      name: "",
-      age: "",
-      email: ""
-    });
-  };
-
-  handleDelete = e => {
-    e.preventDefault();
-    console.log(e.target.value);
-    axios
-      .delete(`http://localhost:5000/friends/${e.target.value}`)
-      .then(response => {
-        this.setState({ friends: response.data });
-        console.log(this.state.friends);
-      });
-  };
-
-  render() {
-    return (
+function FriendsList(props) {
+  console.log(props);
+  return (
+    <div>
       <div>
-        <div>
-          {this.state.friends.map(friend => (
-            <div>
-              <h2 key={friend.id}>{friend.name}</h2>
-              <button value={friend.id} onClick={this.handleDelete}>
-                X
-              </button>
-            </div>
-          ))}
-        </div>
+        {props.friends.map(friend => (
+          <div>
+            <h2 key={friend.id}>{friend.name}</h2>
+            <button value={friend.id} onClick={props.handleDelete}>
+              X
+            </button>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default FriendsList;
